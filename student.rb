de24a98da39e6ec8647de96361db05babd7521ce
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-07-06 21:40:21 +1000

    Fix linter errors

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+
 class Student < Person
   def initialize(age, classroom:, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
