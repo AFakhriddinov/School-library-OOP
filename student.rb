0fb8752f470f8744d620168cbe9486696d6056e9
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-07-07 21:18:27 +1000

    Add rental class

--- student.rb
+++ student.rb
@@ -5,10 +5,14 @@ class Student < Person
 
   def initialize(age, classroom:, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
