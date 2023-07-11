96721715f620d938a9e1bc3cb35eda31a51fb9f0
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-07-11 22:06:21 +1000

    Add menu.rb file and storage.rb file

--- trim_decorator.rb
+++ trim_decorator.rb
@@ -2,6 +2,6 @@ require './base_decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    super[0..9]
+    @nameable_object.correct_name.length > 10 ? @nameable_object.correct_name[0..9] : @nameable_object.correct_name
   end
 end
